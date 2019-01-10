f89b3f6c0f7bb822472a171bbdd34f8232a56ad6
Erin Dubuc <erin.dubuc9@gmail.com>
2019-01-10 01:58:07 +0000

    created program to draw a tic tac toe board

--- drawBoard.js
+++ drawBoard.js
@@ -2,45 +2,28 @@
 
 let row;
 let column;
-const blank = String.fromCharCode(127);
+const blank = String.fromCharCode(32);
+// const blank = String.fromCharCode(127);
 const vertical = String.fromCharCode(124);
 const horiz = String.fromCharCode(95);
+const plus = String.fromCharCode(43);
 	 
-function drawRow() {
+function drawRows() {
 	
 	for (row = 1; row < 4; row++) {
 		console.log(blank, blank, blank, vertical, blank, blank, blank, vertical, blank, blank, blank);
 	}
 }
 
-// function drawBoard() {
-
+function horizRow() {
+	console.log(horiz, horiz, horiz, horiz, horiz, horiz, horiz, horiz, horiz, horiz, horiz);
+}
+function drawBoard() {
+	for (let i = 0; i < 2; i++) {
+		drawRows();
+		horizRow();
+	}
+	drawRows();
+}
 	
-// 	// start with row 1
-// 	for (row = 1; row < 12; row++) {
-		
-// 		// for each column in the row (12 columns)
-// 		for (column = 1; column < 12; column++) {
-			
-// 			if (!row % 4) {
-				
-// 			}
-			 
-// 			else {
-// 				console.log(horiz);	
-// 			}
-			
-			
-				
-// 			}
-			
-// 		}
-		
-	
-	
-	
-// 	}
-
-// }
-// drawBoard();
-drawRow();
+drawBoard();
---
+++ firrstNonRepeatChar.js
@@ -0,0 +1,9 @@
+
+function firstNonRepeatChar(str) {
+	
+	for (let i = 0; i <= str.length; i++) {
+		let strLower = str.toLowerCase;
+	}
+	
+	
+}
